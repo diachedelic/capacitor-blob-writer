293705861579e3b335eca48458561cb94ca7ef59
James Diacono <james@diacono.com.au>
2020-05-13 16:52:19 +1000

    fix(ios): create fresh server for each attempt

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -17,57 +17,56 @@ public class BlobWriter: CAPPlugin {
   @objc public override func load() {
     // listen for errors only
     GCDWebServer.setLogLevel(4)
-
-    let server = GCDWebServer()
-    
-    server.addDefaultHandler(forMethod: "OPTIONS", request: GCDWebServerRequest.self, processBlock: {request in
-      return self.emptyCorsResponse(statusCode: 200, request: request)
-    })
-    
-    server.addDefaultHandler(forMethod: "PUT", request: GCDWebServerFileRequest.self, processBlock: {request in
-      // unfortunately, we only check Authorization once the request has been
-      // written to disk, which is somewhat insecure (although the file will be
-      // deleted almost immediately)
-      //
-      // When https://github.com/swisspol/GCDWebServer/issues/479 is fixed, we
-      // should use GCDWebServer's built in Basic auth functionality (commented
-      // out)
-      if (request.headers["Authorization"] != self._authPassword) {
-        return self.emptyCorsResponse(statusCode: 401, request: request)
-      }
-      
-      let fileRequest = request as! GCDWebServerFileRequest
-      let src = URL(fileURLWithPath: fileRequest.temporaryPath)
-      let dest = URL(fileURLWithPath: fileRequest.path)
-      
-      // move file into place
-      do {
-        // remove file if it exists
-        try? FileManager.default.removeItem(at: dest)
-        
-        // create intermediate directories
-        if (request.query?["recursive"] == "true") {
-          let destDir = dest.deletingLastPathComponent()
-
-          try FileManager.default.createDirectory(
-            at: destDir,
-            withIntermediateDirectories: true,
-            attributes: nil
-          )
-        }
-
-        try FileManager.default.moveItem(at: src, to: dest)
-      } catch {
-        CAPLog.print("BlobWriter failed to move temp file into place", error)
-        return self.emptyCorsResponse(statusCode: 500, request: request)
-      }
-      
-      return self.emptyCorsResponse(statusCode: 204, request: request)
-    })
     
     // find an available port and listen on it
     var retriesLeft = 5;
     while(_server == nil && retriesLeft > 0) {
+      let server = GCDWebServer()
+      
+      server.addDefaultHandler(forMethod: "OPTIONS", request: GCDWebServerRequest.self, processBlock: {request in
+        return self.emptyCorsResponse(statusCode: 200, request: request)
+      })
+      
+      server.addDefaultHandler(forMethod: "PUT", request: GCDWebServerFileRequest.self, processBlock: {request in
+        // unfortunately, we only check Authorization once the request has been
+        // written to disk, which is somewhat insecure (although the file will be
+        // deleted almost immediately)
+        //
+        // When https://github.com/swisspol/GCDWebServer/issues/479 is fixed, we
+        // should use GCDWebServer's built in Basic auth functionality (commented
+        // out)
+        if (request.headers["Authorization"] != self._authPassword) {
+          return self.emptyCorsResponse(statusCode: 401, request: request)
+        }
+        
+        let fileRequest = request as! GCDWebServerFileRequest
+        let src = URL(fileURLWithPath: fileRequest.temporaryPath)
+        let dest = URL(fileURLWithPath: fileRequest.path)
+
+        // move file into place
+        do {
+          // remove file if it exists
+          try? FileManager.default.removeItem(at: dest)
+
+          // create intermediate directories
+          if (request.query?["recursive"] == "true") {
+            let destDir = dest.deletingLastPathComponent()
+
+            try FileManager.default.createDirectory(
+              at: destDir,
+              withIntermediateDirectories: true,
+              attributes: nil
+            )
+          }
+
+          try FileManager.default.moveItem(at: src, to: dest)
+        } catch {
+          CAPLog.print("BlobWriter failed to move temp file into place", error)
+          return self.emptyCorsResponse(statusCode: 500, request: request)
+        }
+
+        return self.emptyCorsResponse(statusCode: 204, request: request)
+      })
       // select a random, private port
       let port = UInt.random(in: 49151..<65536)
       
