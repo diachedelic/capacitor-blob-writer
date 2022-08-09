e33f50b5fe82b66bec7ad68138a920488e72a544
James Diacono <james@diacono.com.au>
2022-08-09 11:58:13 +1000

    Background
    This commit reconfigures GCDWebServer to start even if the app starts in
    the background. This can happen due to background fetch and location
    updates. Only iOS is affected.

--- example/rollup.config.js
+++ example/rollup.config.js
@@ -1,5 +1,5 @@
 import typescript from 'rollup-plugin-typescript2';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
 
 export default {
   input: 'src/index.ts',
--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
